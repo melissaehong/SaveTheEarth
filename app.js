b9ea620835c1087184eecfcb727c016b8abfe66a
melissaehong <melissahong1@gmail.com>
2021-12-19 23:29:35 -0800

    javascript openai

--- app.js
+++ app.js
@@ -7,9 +7,37 @@ const OpenAI = require('openai-api');
 
 // Load your key from an environment variable or secret management service
 // (do not include your key directly in your code)
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 const openai = new OpenAI(OPENAI_API_KEY);
+
+(async () => {
+    const gptResponse = await openai.complete({
+        engine: 'davinci',
+        prompt: 'this is a test',
+        maxTokens: 5,
+        temperature: 0.9,
+        topP: 1,
+        presencePenalty: 0,
+        frequencyPenalty: 0,
+        bestOf: 1,
+        n: 1,
+        stream: false,
+        stop: ['\n', "testing"]
+    });
+
+    console.log(gptResponse.data);
+})();
+
+(async () => {
+    const gptResponse = await openai.search({
+        engine: 'davinci',
+        documents: ["White House", "hospital", "school"],
+        query: "the president"
+    });
+
+    console.log(gptResponse.data);
+})();
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
