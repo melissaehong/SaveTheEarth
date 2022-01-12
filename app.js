763f16a78a08f887945babeeabef873dcd4af7d4
melissaehong <melissahong1@gmail.com>
2022-01-11 23:08:03 -0800

    adds

--- app.js
+++ app.js
@@ -3,10 +3,46 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const OpenAI = require('openai-api');
+
+// Load your key from an environment variable or secret management service
+// (do not include your key directly in your code)
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+const openai = new OpenAI(OPENAI_API_KEY);
+
+(async () => {
+    const gptResponse = await openai.complete({
+        engine: 'davinci',
+        prompt: 'this is a test',
+        maxTokens: 5,
+        temperature: 0.9,
+        topP: 1,
+        presencePenalty: 0,
+        frequencyPenalty: 0,
+        bestOf: 1,
+        n: 1,
+        stream: false,
+        stop: ['\n', "testing"]
+    });
+
+    console.log(gptResponse.data);
+})();
+
+(async () => {
+    const gptResponse = await openai.search({
+        engine: 'davinci',
+        documents: ["White House", "hospital", "school"],
+        query: "the president"
+    });
+
+    console.log(gptResponse.data);
+})();
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World, I suck at this.');
+  res.end('Hello World' + JSON.stringify(openai));
 });
 
 server.listen(port, hostname, () => {
